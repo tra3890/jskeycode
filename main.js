a35305a4838666fdea973a12cd2486e0a031d56c
CN0075 <cn0075@CN0075.lan>
2019-06-07 13:12:25 +0100

    almost

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-function keycode() {
-    let key = event.keycode;
+function myInput(event) {
 
+    let a = event.key;
+
+    let b = event.which;
+
+    let c = event.code;
+
+
+
+
+
+    document.getElementById("1").innerHTML = "event.key" + a;
+
+    document.getElementById("2").innerHTML = "event.which" + b;
+
+    document.getElementById("3").innerHTML = "event.code" + c;
 }
