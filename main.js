acc4b5a4e4f2e0430eeb1c2a983539d57086699a
CN0075 <cn0075@CN0075.lan>
2019-06-17 15:49:48 +0100

    complete

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+
+// create a function (myInput)
+
 function myInput(event) {
 
-    let a = event.key;
+    //  create variables for the 3 outputs.
+
+    // event.key  is for any key pressed on the keyboard kept simple with a single letter (a)
+
+    let a = event.key;    
+
+    // event.which contains the JS key code for any key pressed
 
     let b = event.which;
 
